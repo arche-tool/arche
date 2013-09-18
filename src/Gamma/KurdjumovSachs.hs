d3a6f5547c4134442333d4f13f3dd344e381d430
Edgar Gomes <talktoedgar@gmail.com>
2013-09-18 22:27:13 +0200

    improve test function by pole figures

--- src/Gamma/KurdjumovSachs.hs
+++ src/Gamma/KurdjumovSachs.hs
@@ -4,26 +4,17 @@ module Gamma.KurdjumovSachs
        ( ksTrans
        ) where
 
+import           System.IO
+
 import           Hammer.Math.Algebra
 import           Hammer.Texture.Orientation
 import           Hammer.Texture.Symmetry
 import           Hammer.Texture.SphereProjection
 
+-- ======================================================================================= 
 
-t1 = let
-  pg = Vec3 1 1 1
-  pa = Vec3 1 1 0
-  theta = angle pa pg
-  axis  = pa &^ pg
-  in mkAxisPair axis (Rad theta)
-
-t2 = let
-  dg = Vec3 (-1)  0  1
-  da = Vec3   1 (-1) 1
-  theta = angle da dg
-  axis  = da &^ dg
-  in mkAxisPair axis (Rad theta)
-
+-- | Kurdjumov-Sachs orientation relationship for phase transformation defined by 24
+-- rotation operations.
 ksTrans :: [Quaternion]
 ksTrans = map (\(h,k,l,o) -> toQuaternion $ mkAxisPair (Vec3 h k l) (Deg o))
   [ ( 1, 1, 2, 90), ( 1, 1,-2, 90), ( 1,-1, 2, 90), ( 1,-1,-2, 90)
@@ -33,15 +24,25 @@ ksTrans = map (\(h,k,l,o) -> toQuaternion $ mkAxisPair (Vec3 h k l) (Deg o))
   , ( 2, 1, 1, 90), ( 2, 1,-1, 90), ( 2,-1, 1, 90), ( 2,-1,-1, 90)
   , (-2, 1, 1, 90), (-2, 1,-1, 90), (-2,-1, 1, 90), (-2,-1,-1, 90)]
 
-testKS :: Vec3 -> [Vec2]
-testKS pole = let
-  q  = mkQuaternion $ Vec4 0 0 0 1
-  ts = map (q +@>) ksTrans
-  qPole  = mkNormal $ activeVecRotation pole q
-  tPoles = map (mkNormal . activeVecRotation pole) ts
-  in map (so3ProjCoord . steroSO3Proj) (qPole : tPoles)
+ksPoles :: (Rot o)=> o -> Vec3 -> [Normal3]
+ksPoles g pole = let
+  q     = toQuaternion g
+  ts    = map (q #<=) ksTrans
+  foo   = mkNormal . activeVecRotation pole
+  in (foo q) : (map foo ts)
 
 
-showTest projs = let
-  out (Vec2 x y) = putStrLn  $ show x ++ "   " ++ show y
-  in mapM_ out projs
+-- | Test KS transformation by plotting pole figures.
+--   > showTest "../PlotScripts/polefigure.data" [Vec3 0 0 1, Vec3 0 1 0, Vec3 0 0 1]
+showTest :: FilePath -> [Vec3] -> IO ()
+showTest file poles = let
+  g = mkEuler (Deg 0) (Deg 54.7) (Deg 45)
+  out h (Vec2 x y) = hPutStrLn h $ show x ++ "   " ++ show y
+  points           = map (map (so3ProjCoord . steroSO3Proj) . ksPoles g) poles
+  writeSet h set   = do
+    hPutStrLn h "\n\n\"pole\""
+    mapM_ (out h) set
+  in do
+    h    <- openFile file WriteMode
+    mapM_ (writeSet h) points
+    hClose h
