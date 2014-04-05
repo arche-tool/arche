6257a1e61db627e79a6b9a50dee24370165885a8
Edgar Gomes <talktoedgar@gmail.com>
2014-04-05 16:38:23 +0200

    find parent orientation from a single grain

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,34 @@
 {-# LANGUAGE RecordWildCards #-}
 module Main where
 
-import           File.ANGReader              (parseANG)
-import           Hammer.Render.VTK.VTKRender (writeUniVTKfile)
-import           Texture.Orientation         (RefFrame (..))
-import           Texture.Symmetry            (Symm (..))
+import qualified Data.Vector as V
+
 import           System.Directory            (doesFileExist)
 
 import           Options.Applicative
 import           System.FilePath
 
+import           Hammer.Render.VTK.VTKRender (writeUniVTKfile)
+import           Hammer.Math.Algebra         (Vec3(..))
+import           Hammer.MicroGraph           (mkGrainID)
+import           File.ANGReader              (parseANG)
+import           Texture.Orientation         (RefFrame (..), (#<=), Deg(..))
+import           Texture.Symmetry            (Symm (..), toFZ)
+
 import           Gamma.OMRender
 import           Gamma.GBRender
 import           Gamma.Grains
+import           Gamma.GammaFinder
+import           Gamma.KurdjumovSachs
+
+import           Texture.SH.HyperSphere
 
 data Gammafier =
   Gammafier
   { ang_input  :: String
   , vtk_output :: Maybe String
-  , tolerance  :: Maybe Double
+  , grain_miso :: Deg
+  , tolerance  :: Double
   } deriving (Show)
 
 gammafier :: Parser Gammafier
@@ -33,11 +43,18 @@ gammafier = Gammafier
       <> short 'o'
       <> metavar "VTK_OUT"
       <> help "VTK visualization.")
-  <*> (optional . option)
+  <*> ((Deg . abs) <$> option
+      (  long "grain-miso"
+      <> short 'm'
+      <> metavar "deg[o]"
+      <> value 15
+      <> help "The default error is 15 deg."))
+  <*> (abs <$> option
       (  long "error-tolerance"
       <> short 'e'
       <> metavar "error[%]"
-      <> help "The default error is 5%.")
+      <> value 0.05
+      <> help "The default error is 5%."))
 
 main :: IO ()
 main = execParser opts >>= run
@@ -58,24 +75,45 @@ run Gammafier{..} = let
   in do
     inOK <- doesFileExist ang_input
     if inOK
-      then renderTest ang_input outName
+      then renderTest grain_miso ang_input outName
       else putStrLn "Invalid input file. Try agian!"
 
-renderTest :: FilePath -> FilePath -> IO ()
-renderTest fin fout = do
+renderTest :: Deg -> FilePath -> FilePath -> IO ()
+renderTest miso fin fout = do
   ang <- parseANG fin
-  let
-    vboxQ  = getVoxBox ang
-    viewGB = [ showGBMiso   Cubic
-             , showGBMisoKS Cubic
-             ]
-    viewOM = [ showOMQI
-             , showOMCI
-             , showOMPhase
-             , showOMIPF    Cubic ND
-             , showGrainIDs Cubic ang
-             ]
-    vtkGB = renderGB viewGB vboxQ
-    vtkOM = renderOM viewOM ang
-  writeUniVTKfile (fout <.> "vti") True vtkOM
-  writeUniVTKfile (fout <.> "vtu") True vtkGB
+  case getGrainID miso Cubic ang of
+    Nothing            -> print "No grain detected!"
+    Just (gids, gtree) -> let
+      vboxQ  = getVoxBox ang
+      viewGB = [ showGBMiso   Cubic
+               , showGBMisoKS Cubic
+               ]
+      viewOM = [ showOMQI
+               , showOMCI
+               , showOMPhase
+               , showOMIPF    Cubic ND
+               , showGrainIDs gids
+               ]
+      (vecQ, vecGID) = getOriGID ang gids
+      vtkGB  = renderGB viewGB vboxQ
+      vtkOM  = renderOM viewOM ang
+      vtkSO2 = renderSO2Points Cubic ND (Vec3 1 0 0) vecGID vecQ
+      vtkSO3 = renderSO3Points Cubic ND vecGID vecQ
+      in do
+        print (V.length vecQ, V.length vecGID)
+        --writeUniVTKfile (fout <.> "vti") True vtkOM
+        --writeUniVTKfile (fout <.> "vtu") True vtkGB
+        writeUniVTKfile (fout <.> "SO2" <.> "vtu") True vtkSO2
+        writeUniVTKfile (fout <.> "SO3" <.> "vtu") True vtkSO3
+        let
+          g        = getGamma ang
+          ggid     = V.singleton $ mkGrainID (-1)
+          as       = V.map (toFZ Cubic . (g #<=)) ksTrans
+          agid     = V.replicate (V.length as) (mkGrainID $ -1)
+          vtkSO2_g = renderSO2Points Cubic ND (Vec3 1 0 0) ggid (V.singleton g)
+          vtkSO3_g = renderSO3Points Cubic ND              ggid (V.singleton g)
+          vtkSO3_a = renderSO3Points Cubic ND              agid as
+        print g
+        writeUniVTKfile (fout <.> "SO2-gamma" <.> "vtu") True vtkSO2_g
+        writeUniVTKfile (fout <.> "SO3-gamma" <.> "vtu") True vtkSO3_g
+        writeUniVTKfile (fout <.> "SO3-alpha" <.> "vtu") True vtkSO3_a
