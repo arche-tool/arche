76eb40507fd17d0fd68f62efb07bf9e84942f54c
Edgar Gomes <talktoedgar@gmail.com>
2014-04-05 16:32:54 +0200

    add renders for orientation space

--- src/Gamma/OMRender.hs
+++ src/Gamma/OMRender.hs
@@ -9,21 +9,25 @@ module Gamma.OMRender
        , showOMIPF
        , showGrainIDs
        , renderOM
+       , renderSO2Points
+       , renderSO3Points
+       , getOriGID
        ) where
 
 import qualified Data.List   as L
 import qualified Data.Vector as V
 
-import           Hammer.MicroGraph   (unGrainID)
+import           Data.Vector         (Vector)
+import           Hammer.MicroGraph   (GrainID, unGrainID)
 
 import           File.ANGReader
 import           Hammer.Render.VTK.VTKRender
+import           Hammer.Math.Algebra
+import           Hammer.VoxBox.Base
 import           Texture.IPF
 import           Texture.Orientation
 import           Texture.Symmetry
-import           Hammer.VoxBox.Base
-
-import           Gamma.Grains
+import           Texture.SH.HyperSphere
 
 -- =======================================================================================
 
@@ -45,9 +49,9 @@ showOMIPF symm ref = let
   foo = unColor . getRGBColor . snd . getIPFColor symm ref . rotation
   in RenderOM ("IPF " ++ show ref) foo
 
-showGrainIDs :: Symm -> EBSDdata -> RenderOM
-showGrainIDs symm ed = let
-  foo = maybe (const 0) (\m -> unGrainID . ((grainID m) V.!)) (getGrainID symm ed)
+showGrainIDs :: VoxBox GrainID -> RenderOM
+showGrainIDs gids = let
+  foo = unGrainID . ((grainID gids) V.!)
   in  RenderAlterMap "GrainID" foo
 
 -- =======================================================================================
@@ -79,3 +83,56 @@ renderOM fs ed@EBSDdata{..}
     addData vtk (RenderAlterMap name f) = let
       func i _ = f i
       in addDataPoints vtk (mkPointAttr name func)
+
+getOriGID :: EBSDdata -> VoxBox GrainID -> (Vector Quaternion, Vector GrainID)
+getOriGID EBSDdata{..} vboxgid = let
+  is   = V.findIndices ((> 0.1) . ci) nodes
+  gids = V.map ((grainID vboxgid) V.!)  is
+  qs   = V.map (rotation . (nodes V.!)) is
+  in (qs, gids)
+
+renderSO3Points :: Symm -> RefFrame -> Vector GrainID -> Vector Quaternion -> VTK Vec3
+renderSO3Points symm ref gids qs = let
+  unColor (RGBColor rgb) = rgb
+
+  vtkBase = renderSO3PointsVTK (V.map quat qs)
+
+  quat  = quaternionToSO3 . toFZ symm
+  color = unColor . getRGBColor . snd . getIPFColor symm ref
+
+  func1 i _    = color (qs V.! i)
+  addColor vtk = addDataPoints vtk (mkPointAttr "IPF colors" func1)
+
+  func2 i _  = unGrainID (gids V.! i)
+  addGID vtk = addDataPoints vtk (mkPointAttr "GrainID" func2)
+
+  in addGID $ addColor vtkBase
+
+renderSO2Points :: Symm -> RefFrame -> Vec3 -> Vector GrainID -> Vector Quaternion -> VTK Vec3
+renderSO2Points symm ref v gidv qs = let
+  unColor (RGBColor rgb) = rgb
+
+  getDirs  = V.singleton . cartToSO2 . activeVecRotation v
+  getColor = unColor . getRGBColor . snd . getIPFColor symm ref
+
+  func i q = let
+    color = getColor q
+    gid   = gidv V.! i
+    dirs  = getDirs q
+    n = V.length dirs
+    in (dirs, V.replicate n gid, V.replicate n color)
+
+  (ds, gs, cs) = V.unzip3 $ V.imap func qs
+  ds' = V.concatMap id ds
+  gs' = V.concatMap id gs
+  cs' = V.concatMap id cs
+
+  vtkBase = renderSO2PointsVTK ds'
+
+  func1 i _    = cs' V.! i
+  addColor vtk = addDataPoints vtk (mkPointAttr "IPF colors" func1)
+
+  func2 i _  = unGrainID (gs' V.! i)
+  addGID vtk = addDataPoints vtk (mkPointAttr "GrainID" func2)
+
+  in addGID $ addColor vtkBase
