d180a28e4d234702c5a0e35d3e0df2e4a914f060
lostbean <talktoedgar@gmail.com>
2020-03-30 23:35:46 -0300

    Save permissions

--- src-server/Handler/SubmitANG.hs
+++ src-server/Handler/SubmitANG.hs
@@ -61,23 +61,19 @@ submitAngHandler user bs = do
       ANG _ -> do
         saveEBSD ebsdBucket ebsdHash bs
     writeHashEBSD user ebsdHash 
+    writePermissionEBSD user ebsdHash 
 
 type GCP = '["https://www.googleapis.com/auth/cloud-platform"]
-
-writeUser :: User -> Google.Google GCP ()
-writeUser user = do
-    let path = "projects/apt-muse-269419/databases/(default)/documents/users/" <> email user
-    void $ Google.send (FireStore.projectsDatabasesDocumentsPatch (toDoc user) path)
 
 writeHashEBSD :: User -> HashEBSD -> Google.Google GCP ()
 writeHashEBSD user (HashEBSD hash)  = do
     let path = "projects/apt-muse-269419/databases/(default)/documents/ebsd/" <> hash
     void $ Google.send (FireStore.projectsDatabasesDocumentsPatch (toDoc user) path)
 
-writeNewUser :: User -> Google.Google GCP ()
-writeNewUser user = do
-    let path = "projects/apt-muse-269419/databases/(default)/documents"
-    void $ Google.send (FireStore.projectsDatabasesDocumentsCreateDocument path "users" (toDoc user))
+writePermissionEBSD :: User -> HashEBSD -> Google.Google GCP ()
+writePermissionEBSD user (HashEBSD hash)  = do
+    let path = "projects/apt-muse-269419/databases/(default)/documents/ebsd/" <> hash <> "/permisson/" <> email user
+    void $ Google.send (FireStore.projectsDatabasesDocumentsPatch (toDoc user) path)
 
 saveEBSD :: StorageBucket -> HashEBSD -> BSL.ByteString -> Google.Google GCP ()
 saveEBSD bucket (HashEBSD angHash) bs = let
@@ -85,3 +81,13 @@ saveEBSD bucket (HashEBSD angHash) bs = let
     vox_key = angHash
     objIns = Storage.objectsInsert (bktText bucket) Storage.object' & Storage.oiName ?~ vox_key
     in void $ Google.upload objIns body
+  
+writeUser :: User -> Google.Google GCP ()
+writeUser user = do
+    let path = "projects/apt-muse-269419/databases/(default)/documents/users/" <> email user
+    void $ Google.send (FireStore.projectsDatabasesDocumentsPatch (toDoc user) path)
+
+writeNewUser :: User -> Google.Google GCP ()
+writeNewUser user = do
+    let path = "projects/apt-muse-269419/databases/(default)/documents"
+    void $ Google.send (FireStore.projectsDatabasesDocumentsCreateDocument path "users" (toDoc user))
