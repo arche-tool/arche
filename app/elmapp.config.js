30a511dfc4f129d791aa80f1baa755a8f91b0bf2
lostbean <talktoedgar@gmail.com>
2020-05-24 00:01:05 -0300

    [BE] add proxy for api to development server on elm-app

---
+++ app/elmapp.config.js
@@ -0,0 +1,14 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = {
+  setupProxy: function(app) {
+    app.use(createProxyMiddleware('/compute.api', {
+        target: 'http://localhost:8080/',
+        pathRewrite: {'^/compute.api' : '/api'}
+    }));
+
+    app.use(createProxyMiddleware('/api', {
+        target: 'http://localhost:8080/'
+    }));
+  }
+};
