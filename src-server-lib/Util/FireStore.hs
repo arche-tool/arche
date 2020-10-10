f26c70e03026b8acb168f7e909ae5c979c421195
lostbean <talktoedgar@gmail.com>
2020-10-10 16:40:32 -0300

    [BE] handle upstream 404

--- src-server-lib/Util/FireStore.hs
+++ src-server-lib/Util/FireStore.hs
@@ -11,11 +11,15 @@ module Util.FireStore
     , module Util.FireStore.Document
     ) where
 
+import Control.Exception            (try)
 import Control.Lens                 ((.~), (<&>))
+import Control.Monad.Catch          (catch, throwM)
 import Control.Monad.IO.Class       (liftIO)
 import Control.Monad.Trans.Resource (runResourceT)
-import System.IO                    (stdout)
+import Network.HTTP.Types.Status    (statusCode)
+import Servant.Server               (err400, err404, err500, err502)
 import Servant                      (Handler, throwError, ServerError(..))
+import System.IO                    (stdout)
 
 import qualified Network.Google           as Google
 import qualified Network.Google.FireStore as FireStore
@@ -24,9 +28,6 @@ import Util.FireStore.Document
 import Util.FireStore.Value
 import Util.Logger
 
-
-import Control.Exception (try)
-
 -- ============== Google Cloud ================
 type GCP = '["https://www.googleapis.com/auth/cloud-platform"]
 
@@ -34,13 +35,8 @@ type GCP = '["https://www.googleapis.com/auth/cloud-platform"]
 runGCPWith :: Google.Google GCP a -> Servant.Handler a
 runGCPWith gcp = do
   result <- liftIO (try action)
-  either handleServerError return result
+  either serverErrorHandler return result
   where
-    handleServerError :: ServerError -> Servant.Handler a 
-    handleServerError err = do
-      liftIO . printLog ERROR $ logMsg ("exited with" :: String) (errHTTPCode err) (errBody err)
-      throwError err
-    
     action = do
       lgr  <- Google.newLogger Google.Info stdout
 
@@ -48,7 +44,25 @@ runGCPWith gcp = do
             (Google.envLogger .~ lgr)
           . (Google.envScopes .~ FireStore.cloudPlatformScope)
 
-      runResourceT $ Google.runGoogle env gcp
+      runResourceT $ Google.runGoogle env (catch gcp googleErrorHandler)
+
+serverErrorHandler :: ServerError -> Servant.Handler a 
+serverErrorHandler err = do
+  let
+    httpCode = errHTTPCode err 
+    logType  = if httpCode >= 400 && httpCode < 500 then INFO else ERROR 
+  liftIO . printLog logType $ logMsg ("Exiting with" :: String) httpCode ("(" <> errReasonPhrase err <> ")")
+  throwError err
+
+googleErrorHandler :: Google.Error -> Google.Google GCP a
+googleErrorHandler err = case err of
+  Google.TransportError httpException  -> logGGError (logMsg $ show httpException)  >> throwM err500
+  Google.SerializeError serializeError -> logGGError (logMsg $ show serializeError) >> throwM err500 
+  Google.ServiceError   serviceError
+    | httpStatus == 404 -> logGGInfo  (logMsg $ show serviceError) >> throwM err404
+    | httpStatus == 400 -> logGGInfo  (logMsg $ show serviceError) >> throwM err400
+    | otherwise         -> logGGError (logMsg $ show serviceError) >> throwM err502
+    where httpStatus = statusCode (Google._serviceStatus serviceError)
 
 -- ============== Document store ================
 toDoc :: ToDocValue a => a -> FireStore.Document
